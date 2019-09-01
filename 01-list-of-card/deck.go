a3d61078fe058f52a40ae331a145c8985f8ce9c0
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-09-01 14:20:06 +0700

    update cards

--- 01-list-of-card/deck.go
+++ 01-list-of-card/deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 type deck []string
 
@@ -19,12 +24,36 @@ func newDeck() deck {
 	return cards
 }
 
+// receiver function
 func (cards deck) show() {
 	for i, card := range cards {
 		fmt.Println(i, card)
 	}
 }
 
+// slices
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+// join
+func (cards deck) toString() string {
+	temp := []string(cards)
+	return strings.Join(temp, ",")
+}
+
+func (cards deck) saveToFile(filename string) error {
+	bytes := []byte(cards.toString())
+	return ioutil.WriteFile(filename, bytes, 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	temp := string(bytes)
+	return strings.Split(temp, ",")
 }
--- 01-list-of-card/main.go
+++ 01-list-of-card/main.go
@@ -1,20 +1,33 @@
 package main
 
 func main() {
+	// explicit variable type declaration
 	// var card string = "Ace of Spades"
+
+	// right hand variable declaration
 	// card := "Ace of Spades"
 	// card = "Five of Diamonds"
 	// card := newCard()
 
+	// custom types with slices / array
 	// cards := deck{"Ace of Diamonds", newCard()}
 	// cards = append(cards, "Six of Spades")
+	// cards.show()
 
+	// type conversion
+	// greeting := "hola"
+	// fmt.Println([]byte(greeting))
+
+	// method declatarion and receiver function
 	cards := newDeck()
-	hand, remaining := deal(cards, 5)
+	hand, _ := deal(cards, 5)
+	// hand.show()
+	// remaining.show()
 
-	cards.show()
-	hand.show()
-	remaining.show()
+	// save to and read from file
+	hand.saveToFile("cards")
+	newDeck := newDeckFromFile("cards")
+	newDeck.show()
 }
 
 func newCard() string {
